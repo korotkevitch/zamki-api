462cd780c2cec65b0ab4556548d7b3b804a9c568
Igor Korotkevitch <i.korotkevitch@gmail.com>
2022-11-10 20:10:48 +0300

    Added Djoser & JWT

--- zamki/settings.py
+++ zamki/settings.py
@@ -153,8 +153,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
         # 'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
-        # 'rest_framework_simplejwt.authentication.JWTAuthentication',
+        # 'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
 
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
--- zamki/urls.py
+++ zamki/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
+from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView,)
 
 
 
@@ -26,5 +27,10 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html')),
 
     path('api-auth/', include('rest_framework.urls')),
+    re_path(r'^auth/', include('djoser.urls')),
+    re_path(r'^auth/', include('djoser.urls.jwt')),
+
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
